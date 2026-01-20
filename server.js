79172d3333b5770748387ac901e53ee13af8f727
Coderofcoders70 <Lakshayas918@gmail.com>
2026-01-20 13:00:25 +0530

    Get operation successfully completed

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
+const cors = require('cors');
+const app = express();
+
+// Use the data from model 
+const data = {}; // Empty object
+data.employees = require('./model/employees.json');
 
 // cors
+app.use(cors());
 
 // middleware
+app.use(express.json());
 
 // routes
+app.get('/', (req, res) => {
+    res.json(data.employees);
+});
 
-console.log("Hello World! Setting up enviornment");
+// Port
+app.listen(3000);
+
+console.log("Listening at Port 3000...");
